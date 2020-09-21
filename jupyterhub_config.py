bf712d8644fef74620d961c14ffd0a3dd5f0a44a
riderx <martindonadieu@gmail.com>
2020-09-21 11:09:03 +0200

    push config

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -1,73 +1,127 @@
-# Copyright (c) Jupyter Development Team.
+# Copyright (c) Naas Development Team.
 # Distributed under the terms of the Modified BSD License.
 
-# Configuration file for JupyterHub
 import os
 
 c = get_config()
+
+c.NotebookApp.ResourceUseDisplay.track_cpu_percent = True
+c.NotebookApp.ResourceUseDisplay.mem_warning_threshold = 0.1
+c.NotebookApp.ResourceUseDisplay.cpu_warning_threshold = 0.1
 
 # We rely on environment variables to configure JupyterHub so that we
 # avoid having to rebuild the JupyterHub container every time we change a
 # configuration parameter.
 
 # Spawn single-user servers as Docker containers
-c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+c.JupyterHub.spawner_class = 'kubespawner.KubeSpawner'
 c.JupyterHub.service_tokens = {
     'secret-token': os.environ.get('ADMIN_API_TOKEN', 'SHOULD_BE_CHANGED'),
 }
-# Spawn containers from this image
-c.DockerSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
-c.DockerSpawner.pull_policy = 'always'
+
+c.KubeSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
+c.KubeSpawner.image_pull_policy = 'Always'
 
 # JupyterHub requires a single-user instance of the Notebook server, so we
 # default to using the `start-singleuser.sh` script included in the
 # jupyter/docker-stacks *-notebook images as the Docker run command when
 # spawning containers.  Optionally, you can override the Docker run command
 # using the DOCKER_SPAWN_CMD environment variable.
-c.DockerSpawner.environment = {
-    'PUBLIC_DATASCIENCE': os.environ.get('PUBLIC_DATASCIENCE', ''),
-    'DK_API': os.environ.get('DK_API', ''),
-    'DK_API_PREPROD': os.environ.get('DK_API_PREPROD', ''),
+c.KubeSpawner.environment = {
+    'JUPYTERHUB_URL': os.environ.get('JUPYTERHUB_URL', ''),
     'PUBLIC_DK_API': os.environ.get('PUBLIC_DK_API', ''),
-    'PUBLIC_DK_API_PREPROD': os.environ.get('PUBLIC_DK_API_PREPROD', ''),
-    'HC_API': os.environ.get('HC_API', ''),
     'TC_API_SCREENSHOT': os.environ.get('TC_API_SCREENSHOT', ''),
     'ALLOWED_IFRAME': os.environ.get('ALLOWED_IFRAME', ''),
-    'TZ': 'Europe/Paris',
+    'TZ': os.environ.get('TZ', 'Europe/Paris')
 }
-spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', 'start-singleuser.sh')
-c.DockerSpawner.extra_create_kwargs.update({'command': spawn_cmd})
-# Connect containers to this Docker network
-network_name = os.environ['DOCKER_NETWORK_NAME']
-c.DockerSpawner.use_internal_ip = True
-c.Spawner.mem_limit = '4G'
-c.DockerSpawner.network_name = network_name
-# Pass the network name as argument to spawned containers
-c.DockerSpawner.extra_host_config = {'network_mode': network_name}
+
+c.KubeSpawner.cpu_guarantee = os.environ.get('KUBE_CPU_GUAR', 0.3)
+c.KubeSpawner.cpu_limit = os.environ.get('KUBE_CPU_LIMIT', 1.0)
+c.KubeSpawner.mem_limit = os.environ.get('KUBE_MEM_LIMIT', '4G')
+c.KubeSpawner.mem_guarantee = os.environ.get('KUBE_MEM_GUAR', '500M')
+
 # Explicitly set notebook directory because we'll be mounting a host volume to
 # it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
 # user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
 # We follow the same convention.
-notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/ftp/ftp'
-c.DockerSpawner.notebook_dir = notebook_dir
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/ftp'
+c.KubeSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
-# notebook directory in the container
-c.DockerSpawner.volumes = {
-    '/app/ftp/{username}': {"bind": notebook_dir, "mode": "z"},
+
+c.KubeSpawner.volumes = [
+    {
+        'name': 'nfs-root',
+        'nfs': {
+            'server': os.environ.get('VOLUME_SERVER', 'fs-b87bd009.efs.eu-west-3.amazonaws.com'),
+            'path': '/'
+        }
+    }
+]
+
+c.KubeSpawner.volume_mounts = [
+    {
+        'name': 'nfs-root',
+        'mountPath': os.environ.get('DOCKER_NOTEBOOK_DIR'),
+        'subPath':  os.environ.get('KUBE_NAMESPACE', 'prod') + '/ftpusers/{username}'
+    }
+]
+
+# This is used to set proper rights on NFS mount point.
+c.KubeSpawner.lifecycle_hooks = {
+    "postStart": {
+        "exec": {
+            "command": ["/bin/sh", "-c", f"chown -R 21:21 {os.environ.get('DOCKER_NOTEBOOK_DIR')}"]
+        }
+    }
 }
-# Remove containers once they are stopped
-c.DockerSpawner.remove_containers = True
+
+
+c.KubeSpawner.extra_pod_config = {
+    "subdomain": "jupyter-single-user",
+    "hostname": "jupyter-{username}",
+    "affinity": {
+        "nodeAffinity": {
+        "requiredDuringSchedulingIgnoredDuringExecution": {
+            "nodeSelectorTerms": [
+            {
+                "matchExpressions": [
+                {
+                    "key": "jupyterNodeGroup",
+                    "operator": "In",
+                    "values": [
+                    "true"
+                    ]
+                }
+                ]
+            }
+            ]
+        }
+        }
+    },
+    "tolerations": [
+        {
+        "key": "jupyter",
+        "operator": "Equal",
+        "value": "true",
+        "effect": "NoSchedule"
+        }
+    ]
+}
+
+c.KubeSpawner.extra_labels = {
+    "name": "jupyter-single-user"
+}
 # For debugging arguments passed to spawned containers
-c.DockerSpawner.debug = True
+c.KubeSpawner.debug = True
+c.KubeSpawner.start_timeout = 120
 
 # User containers will access hub by container name on the Docker network
 c.JupyterHub.hub_ip = os.environ.get('HOST', '0.0.0.0')
-c.JupyterHub.hub_port = os.environ.get('PORT', 8080)
+c.JupyterHub.hub_port = os.environ.get('PORT', 8081)
 
-# Authenticate testing module, you need to comment GitHub OAuth to use it
-#c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.KubeSpawner.hub_connect_ip = 'hub'
 
-# Authenticate users with GitHub OAuth
+# Authenticate users with local
 c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
 c.Authenticator.check_common_password = True
 c.Authenticator.minimum_password_length = 10
@@ -76,7 +130,7 @@ c.Authenticator.minimum_password_length = 10
 data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
 
 c.JupyterHub.cookie_secret_file = os.path.join(data_dir,
-                                               'jupyterhub_cookie_secret')
+                                            'jupyterhub_cookie_secret')
 
 c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
     host=os.environ['POSTGRES_HOST'],
